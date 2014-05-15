426bfc1e7d91dd0755df030122031431df246c63
Taichi <taichi@curipha.jp>
2014-05-15 13:35:17 +0900

    Autodetect "stylish.sqlite" from Firefox's user profile

--- _export.sh
+++ _export.sh
@@ -3,6 +3,9 @@
 # Stylish extractor
 # * Extract stylish.sqlite to *.user.css files
 
+FXPROFILE=${USERPROFILE//\\/\/}/AppData/Roaming/Mozilla/Firefox/Profiles
+STYLISH_LIST=list.tmp
+
 SQLITE=sqlite3
 SQLITEOPTION=-list
 STYLISH=stylish.sqlite
@@ -19,8 +22,17 @@ if [ ! -x "`which ${SQLITE} 2> /dev/null`" ]; then
   exit 1
 fi
 
-if [ ! -f ${STYLISH} ]; then
-  echo "ERR: Stylish file (${STYLISH}) is not exists."
+# Get stylish.sqlite from Firefox's user profile directory
+if [ -e ${STYLISH_LIST} ]; then
+  rm ${STYLISH_LIST}
+fi
+
+find "${FXPROFILE}" -type f -iname "${STYLISH}" -fprint "${STYLISH_LIST}"
+
+if [ ! -s ${STYLISH_LIST} ]; then
+  rm ${STYLISH_LIST}
+
+  echo "ERR: Cannot find ${STYLISH} from your Firefox profile directory."
   exit 1
 fi
 
@@ -34,28 +46,53 @@ if [ ! -e ${DISABLEDIR} ]; then
 fi
 
 
-# Extract userstyles from stylish.sqlite
-${SQLITECMD} 'select id from styles where url is null order by id' \
+# Execute each stylish.sqlite
+cat ${STYLISH_LIST} \
   | \
-  while read id; do
-    NAME=`${SQLITECMD} "select name from styles where id = ${id}"`
-    DESC=`echo ${NAME} | perl -pe 's/^(?!\[XUL\])(\[[^\]]+\])\s*(.+)$/\2/g'`
+  while read file; do
 
-    ENABLED=`${SQLITECMD} "select enabled from styles where id = ${id}"`
-    if [ ${ENABLED} = "1" ]; then
-      DIR=./
-    else
-      DIR=./${DISABLEDIR}/
+    echo Opening "${file}"...
+
+    # Remove older stylish.sqlite
+    if [ -e ${STYLISH} ]; then
+      rm "${STYLISH}"
     fi
 
-    BASE=`echo ${DESC} | sed -e 's/ \+/_/g' | tr -cd 'A-Za-z0-9._-'`
-    FILE="${DIR}${BASE}${USERCSSEXT}"
+    # Copy stylish.sqlite
+    cp "${file}" "${STYLISH}"
 
-    echo Processing "${NAME}"...
+    # Extract userstyles from stylish.sqlite
+    ${SQLITECMD} 'select id from styles where url is null order by id' \
+      | \
+      while read id; do
+        NAME=`${SQLITECMD} "select name from styles where id = ${id}"`
+        DESC=`echo ${NAME} | perl -pe 's/^(?!\[XUL\])(\[[^\]]+\])\s*(.+)$/\2/g'`
 
-    echo "/* ${DESC} */" > ${FILE}
-    ${SQLITECMD} "select code from styles where id = ${id}" | tr -d "\r" >> ${FILE}
+        ENABLED=`${SQLITECMD} "select enabled from styles where id = ${id}"`
+        if [ ${ENABLED} = "1" ]; then
+          DIR=./
+        else
+          DIR=./${DISABLEDIR}/
+        fi
 
-    chmod 0644 ${FILE}
-  done
+        BASE=`echo ${DESC} | sed -e 's/ \+/_/g' | tr -cd 'A-Za-z0-9._-'`
+        FILE="${DIR}${BASE}${USERCSSEXT}"
+
+        echo Processing "${NAME}"...
+
+        echo "/* ${DESC} */" > ${FILE}
+        ${SQLITECMD} "select code from styles where id = ${id}" | tr -d "\r" >> ${FILE}
+
+        chmod 0644 ${FILE}
+      done
+
+done
+
+# Remove temporary file
+if [ -e ${STYLISH} ]; then
+  rm ${STYLISH}
+fi
+if [ -e ${STYLISH_LIST} ]; then
+  rm ${STYLISH_LIST}
+fi
 
